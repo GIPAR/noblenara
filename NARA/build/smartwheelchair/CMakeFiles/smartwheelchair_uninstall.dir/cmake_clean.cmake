5e4f555eac05bb136e43cc262e10708b74be83f8
David Manhães <108944025+ReadingKnightofJesus@users.noreply.github.com>
2025-09-01 14:00:57 -0300

    Delete NARA directory

--- NARA/build/smartwheelchair/CMakeFiles/smartwheelchair_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/smartwheelchair_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/smartwheelchair_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
