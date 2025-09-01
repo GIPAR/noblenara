f5d697316027469c9ae7b8dbe9ccbe967c7cfcb8
David Manhães <108944025+ReadingKnightofJesus@users.noreply.github.com>
2025-09-01 15:12:43 -0300

    Add files via upload

---
+++ NARA/src/smartwheelchair/build/smartwheelchair/CMakeFiles/smartwheelchair_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smartwheelchair_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/smartwheelchair_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
